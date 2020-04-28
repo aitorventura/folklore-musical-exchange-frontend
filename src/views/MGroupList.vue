0b1c5cc487c45b53f9b012c0303feb0de108dcde
GuillermoHRT <guillermohrt@gmail.com>
2020-04-28 22:07:15 +0200

    Imagen

--- src/views/MGroupList.vue
+++ src/views/MGroupList.vue
@@ -1,108 +1,104 @@
 <template>
-  <body>
-    <br />
-    <br />
-    <br />
-    <div class="container-fluid">
-      <div class="text-center">
-        <h1>Musical Group List</h1>
+<body>
+  <br />
+  <br />
+  <br />
+  <div class="container-fluid">
+    <div class="text-center">
+      <h1>Musical Group List</h1>
 
-        <div v-if="mgroups.length === 0">
-          <h2>No user found at the moment</h2>
-        </div>
+      <div v-if="mgroups.length === 0">
+        <h2>No user found at the moment</h2>
       </div>
-
-      <!-- <div class="row"> -->
-      <div class>
-        <div>
-          <ejs-textbox
-            id="textbox"
-            v-model="searchName"
-            floatLabelType="Never"
-            showClearButton="true"
-            placeholder="Search by name"
-            width="200px"
-          ></ejs-textbox>
-
-          <ejs-textbox
-            id="textbox"
-            v-model="searchCity"
-            floatLabelType="Never"
-            showClearButton="true"
-            placeholder="Search by city"
-            width="200px"
-          ></ejs-textbox>
-
-          <ejs-multiselect
-            :id="searchType"
-            v-model="searchType"
-            :dataSource="nameTypes"
-            placeholder="Select a type"
-            mode="CheckBox"
-            :fields="fields"
-            :showSelectAll="showSelectAll"
-            selectAllText="Seleccionar todos"
-            unSelectAllText="Deseleccionar todos"
-            width="200px"
-          ></ejs-multiselect>
-        </div>
-
-        <table class="table table-bordered">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">Name</th>
-              <th scope="col">Email</th>
-              <th scope="col">Username</th>
-              <th scope="col">City</th>
-              <th scope="col">NameType</th>
-              <th scope="col">Description</th>
-              <th scope="col">Members</th>
-              <th scope="col">Image</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="mgroup in filterName" :key="mgroup.id">
-              <td>{{ mgroup.name }}</td>
-              <td>{{ mgroup.email }}</td>
-              <td>{{ mgroup.username }}</td>
-              <td>{{ mgroup.city }}</td>
-              <td>{{ mgroup.nameType }}</td>
-              <td>{{ mgroup.description }}</td>
-              <td>{{ mgroup.members }}</td>
-              <td>
-                <img v-bind:src="mgroup.image" />
-                <!--<img v-bind: charset="UTF-8" src="mgroup.image" />-->
-              </td>
-              <td>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group" style="margin-bottom: 20px;">
-                    <router-link
-                      :to="{ name: 'EditMG', params: { id: mgroup.id } }"
-                      class="btn btn-sm btn-outline-secondary"
-                      >Edit MGroup</router-link
-                    >
-                    <router-link
-                      :to="{ name: 'GetMG', params: { id: mgroup.id } }"
-                      class="btn btn-sm btn-outline-primary"
-                      >Show MGroup</router-link
-                    >
-
-                    <button
-                      class="btn btn-sm btn-outline-secondary"
-                      v-on:click="deleteMGroup(mgroup.id)"
-                    >
-                      Delete MGroup
-                    </button>
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <!-- </div> -->
     </div>
-  </body>
+
+    <!-- <div class="row"> -->
+    <div class>
+      <div>
+        <ejs-textbox
+          id="textbox"
+          v-model="searchName"
+          floatLabelType="Never"
+          showClearButton="true"
+          placeholder="Search by name"
+          width="200px"
+        ></ejs-textbox>
+
+        <ejs-textbox
+          id="textbox"
+          v-model="searchCity"
+          floatLabelType="Never"
+          showClearButton="true"
+          placeholder="Search by city"
+          width="200px"
+        ></ejs-textbox>
+
+        <ejs-multiselect
+          :id="searchType"
+          v-model="searchType"
+          :dataSource="nameTypes"
+          placeholder="Select a type"
+          mode="CheckBox"
+          :fields="fields"
+          :showSelectAll="showSelectAll"
+          selectAllText="Seleccionar todos"
+          unSelectAllText="Deseleccionar todos"
+          width="200px"
+        ></ejs-multiselect>
+      </div>
+
+      <table class="table table-bordered">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Username</th>
+            <th scope="col">City</th>
+            <th scope="col">NameType</th>
+            <th scope="col">Description</th>
+            <th scope="col">Members</th>
+            <th scope="col">Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="mgroup in filterName" :key="mgroup.id">
+            <td>{{ mgroup.name }}</td>
+            <td>{{ mgroup.email }}</td>
+            <td>{{ mgroup.username }}</td>
+            <td>{{ mgroup.city }}</td>
+            <td>{{ mgroup.nameType }}</td>
+            <td>{{ mgroup.description }}</td>
+            <td>{{ mgroup.members }}</td>
+            <td>
+              <img v-bind:src="mgroup.image" />
+              <!--<img v-bind: charset="UTF-8" src="mgroup.image" />-->
+            </td>
+            <td>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group" style="margin-bottom: 20px;">
+                  <router-link
+                    :to="{ name: 'EditMG', params: { id: mgroup.id } }"
+                    class="btn btn-sm btn-outline-secondary"
+                  >Edit MGroup</router-link>
+                  <router-link
+                    :to="{ name: 'GetMG', params: { id: mgroup.id } }"
+                    class="btn btn-sm btn-outline-primary"
+                  >Show MGroup</router-link>
+
+                  <button
+                    class="btn btn-sm btn-outline-secondary"
+                    v-on:click="deleteMGroup(mgroup.id)"
+                  >Delete MGroup</button>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- </div> -->
+  </div>
+</body>
 </template>
 
 <script>
@@ -126,7 +122,7 @@ export default {
       nameTypes: [],
       fields: { text: "name", value: "name" },
       /*maximumSelectionLength: "",*/
-      showSelectAll: true,
+      showSelectAll: true
     };
   },
   created() {
@@ -135,23 +131,23 @@ export default {
   },
   computed: {
     filterName() {
-      return this.mgroups.filter((mgroup) => {
+      return this.mgroups.filter(mgroup => {
         return (
           mgroup.name.toLowerCase().match(this.searchName.toLowerCase()) &&
           mgroup.city.toLowerCase().match(this.searchCity.toLowerCase()) &&
           this.isType(mgroup.nameType)
         );
       });
-    },
+    }
   },
   methods: {
     fetchMGroups() {
       axios
         .get(`${server.baseURL}/musicalgroup`)
-        .then((data) => (this.mgroups = data.data));
+        .then(data => (this.mgroups = data.data));
     },
     deleteMGroup(id) {
-      axios.delete(`${server.baseURL}/musicalgroup/${id}`).then((data) => {
+      axios.delete(`${server.baseURL}/musicalgroup/${id}`).then(data => {
         if (data.data) {
           window.location.reload();
         } else {
@@ -165,7 +161,7 @@ export default {
       axios
         .get(`${server.baseURL}/type`)
         .then(
-          (data) => (
+          data => (
             (this.nameTypes = data.data),
             (this.maximumSelectionLength = data.data.length)
           )
@@ -182,10 +178,11 @@ export default {
       } else {
         return true;
       }
-    },
-  },
+    }
+  }
 };
 </script>
+
 
 <style>
 @import url(http://cdn.syncfusion.com/ej2/material.css);
